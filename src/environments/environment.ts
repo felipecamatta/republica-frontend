966882f5fc75cd404b41052cd121ef0670ccc807
Lucas <lucasmxavier1@gmail.com>
2020-03-18 18:09:59 -0300

    buscar morador/republica

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://republica-backend.herokuapp.com/',
   production: false
 };
 
